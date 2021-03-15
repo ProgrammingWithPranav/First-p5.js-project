cb5825f5f5909327d7ec06e577cd39117088d3eb
Prana Tantri <67408817+ProgrammingWithPranav@users.noreply.github.com>
2021-03-15 12:59:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+function setup() {
+    canvas = createCanvas(640, 480);
+    canvas.position(150, 150);
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    image(video, 230, 150, 220, 200)
+
+    fill(255, 0, 0);
+    stroke(255, 0, 0);
+    circle(50, 50, 80);
+
+    fill(0, 128, 0);
+    stroke(0, 128, 0);
+    rect(90, 40, 460, 20);
+
+    rect(90, 420, 460, 20);
+
+    fill(255, 0, 0);
+    stroke(255, 0, 0);
+    circle(50, 420, 80);
+
+    fill(0, 128, 0);
+    stroke(0, 128, 0);
+    rect(50, 390, 20, -300);
+
+    fill(0, 128, 0);
+    stroke(0, 128, 0);
+    rect(580, 390, 20, -300);
+
+    fill(255, 0, 0);
+    stroke(255, 0, 0);
+    circle(590, 420, 80);
+
+    fill(255, 0, 0);
+    stroke(255, 0, 0);
+    circle(590, 50, 80);
+}
+
+function take_snapshot() {
+    save("photo.png");
+}
